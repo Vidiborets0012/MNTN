85293087aa86700342a838e35179dee9fe80e753
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-03-25 11:12:05 +0200

    add js for site-list

--- js/main.js
+++ js/main.js
@@ -5,3 +5,26 @@ menuBtn.addEventListener('click', () => {
     menuList.classList.toggle('menu__list-open')
 
 });
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            const activeId = entry.target.id;
+
+            document.querySelectorAll('.site-list__item').forEach(item => {
+                item.classList.remove('site-list__item--active');
+            });
+
+            const activeLink = document.querySelector(`.site-list__link[href="#${activeId}"]`);
+
+            if (activeLink) {
+                activeLink.closest('.site-list__item').classList.add('site-list__item--active');
+            }
+        }
+
+     });
+}, {threshold: 0.5});
+
+document.querySelectorAll('#slide-0, #slide-1, #slide-2, #slide-3').forEach(element => {
+    observer.observe(element)
+});
